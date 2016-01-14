a73e65ec43e6894c6b653100302f0aa1e64fd3a9
Rune Glerup <rune.glerup@gmail.com>
2016-01-14 17:42:28 +0100

    Add another test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,17 @@ var playerstore = require('../player-store')
 
 var assert = require('assert');
 describe('player-store', function() {
-  describe('demo', function () {
-    it('should work', function () {
-        playerstore.save("a", { name:"b" } );
-        
-        assert.equal("b", playerstore.read("a").name);
+  describe('when a player is stored', function () {
+    var originalPlayer = { name : "b" };
+    var address = 'the-address';
+    playerstore.save(address, originalPlayer);
+    it('should retain its name when retrieved', function() {
+      var retrievedPlayer = playerstore.read(address);
+      assert.equal(originalPlayer.name, retrievedPlayer.name);
+    });
+    it('should contain the address on which it was stored', function() {
+      var retrievedPlayer = playerstore.read(address);
+      assert.equal(address, retrievedPlayer.address);
     });
   });
 });
