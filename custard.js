9e85639aa2d29bcf2dd8de59f1990585eaa15a4c
Rune Glerup <rune.glerup@gmail.com>
2016-01-14 16:04:17 +0100

    Add /players endpoint

--- custard.js
+++ custard.js
@@ -6,6 +6,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/players/:address', function(req, res, next) {
+  var address = req.params.address;
+  res.json({message:'Hello, ' + address});
+  next();
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
